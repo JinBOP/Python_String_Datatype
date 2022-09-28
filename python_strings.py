98952dd01c004122980be27f475133ba2fb219b4
Jin <novhunter@gmail.com>
2022-09-27 18:09:21 -0600

    Created Variables for name

--- python_strings.py
+++ python_strings.py
@@ -2,13 +2,18 @@
 #   - Create the following variables
 #   - my_first_name
 #       -set this equal to your first name
+my_first_name = 'Jin'
+myFirstName = 'Jin'
+fname = 'Jin'
 #   - my_last_name
 #       -set this equal to your last name
+my_last_name = 'Starks'
 #   - my_year_of_birth
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
+my_year_of_birth = 2000
 #   - current_year
 #       -set this equal to 2020
-
+current_year = 2020
 
 
 
