81ef1e6716f95b1af9a4547b1f2315d99af40d32
Jin <novhunter@gmail.com>
2022-09-29 18:20:18 -0600

    escape characters used

--- python_strings.py
+++ python_strings.py
@@ -13,7 +13,7 @@ my_last_name = 'Starks'
 my_year_of_birth = 2000
 #   - current_year
 #       -set this equal to 2020
-current_year = 2020
+current_year = 2022
 
 # TODO String Indexing
 #   - Print the following items (one per line) (print using variables)
@@ -23,32 +23,37 @@ current_year = 2020
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - last two letter of your last name (use the -index)
-print (my_first_name)
-print (my_last_name)
-print (my_first_name[0])
-print (my_last_name[-5])
-print (my_first_name[0:2])
-print (my_last_name[-3:-1])
+## print (my_first_name)
+## print (my_last_name)
+## print (my_first_name[0])
+## print (my_last_name[-5])
+## print (my_first_name[0:2])
+## print (my_last_name[-3:-1])
 
 #TODO Combining Strings
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
-print (my_first_name, my_last_name)
-print (my_first_name, '\n' * 6)
+## print (my_first_name, my_last_name)
+## print ((my_first_name + '\n') * 6)
 
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
-
-
+## birth_year_statement = '{} {} was born in {}'
+## celebration_statement = '{} enjoyed celebrating {}'
+## print (birth_year_statement.format(my_first_name,my_last_name,my_year_of_birth))
+## print (birth_year_statement.format(my_first_name,my_first_name,my_year_of_birth), celebration_statement.format(my_first_name,current_year))
 
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
 #       - tab last name current year
-
+escape_example = "{}\'s birth year is {}"
+print(escape_example.format(my_first_name,my_year_of_birth))
+escape_example_two = "\t {} {}"
+print(escape_example_two.format(my_last_name,current_year))
 
 # TODO String methods
 #   - Print the following items (one per line) (print using variables)
